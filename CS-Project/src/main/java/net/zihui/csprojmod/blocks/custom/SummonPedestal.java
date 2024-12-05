a29b89db450ec86d7fba5d79a000403d15d70ffc
NatureNitaso <zihuilei001@gmail.com>
2024-12-05 13:49:24 -0800

    Fixed Item not being consumed
    Made it so that upon interacting, consume the heart of the sea and explode

--- CS-Project/src/main/java/net/zihui/csprojmod/blocks/custom/SummonPedestal.java
+++ CS-Project/src/main/java/net/zihui/csprojmod/blocks/custom/SummonPedestal.java
@@ -15,6 +15,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.items.ItemStackHandler;
 import org.jetbrains.annotations.NotNull;
 
 public class SummonPedestal extends Block {
@@ -29,11 +30,13 @@ public class SummonPedestal extends Block {
     public @NotNull InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
         // Makes a variable that identifies the held item of the player interacting with the block
         ItemStack held = player.getItemInHand(hand);
+        int index = player.getItemInHand(hand).getCount();
 
         // Checks to see if item matchs with it's requirements. 1. is running on server ride
         // 2. held item is equal to summon item
         // 3.
         if (!level.isClientSide() && held.getItem() == Items.HEART_OF_THE_SEA){
+            player.getItemInHand(hand).setCount(index - 1); //Takes away 1 item upon consumption
             level.explode(player, pos.getX(), pos.getY(), pos.getZ(), 4.0F, true, Explosion.BlockInteraction.NONE);
             return InteractionResult.CONSUME;
         }
