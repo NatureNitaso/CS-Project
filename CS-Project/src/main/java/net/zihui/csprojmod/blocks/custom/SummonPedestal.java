42e5bb2a668e253cffe817005888429cc4420a58
NatureNitaso <zihuilei001@gmail.com>
2024-12-05 13:15:50 -0800

    Created SummonPedestal Block
    -Created Summon Pedestal Block
    -Explodes upon interaction(right click) with heart of the sea
    -Can be placed

---
+++ CS-Project/src/main/java/net/zihui/csprojmod/blocks/custom/SummonPedestal.java
@@ -0,0 +1,52 @@
+package net.zihui.csprojmod.blocks.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Explosion;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.NotNull;
+
+public class SummonPedestal extends Block {
+    public static Items summon;
+
+    public SummonPedestal(Block.Properties properties) {
+        super(properties);
+    }
+    // Overrides the interaction result method (what happens upon interacting
+
+    @Override
+    public @NotNull InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        // Makes a variable that identifies the held item of the player interacting with the block
+        ItemStack held = player.getItemInHand(hand);
+
+        // Checks to see if item matchs with it's requirements. 1. is running on server ride
+        // 2. held item is equal to summon item
+        // 3.
+        if (!level.isClientSide() && held.getItem() == Items.HEART_OF_THE_SEA){
+            level.explode(player, pos.getX(), pos.getY(), pos.getZ(), 4.0F, true, Explosion.BlockInteraction.NONE);
+            return InteractionResult.CONSUME;
+        }
+
+
+        return super.use(state, level, pos, player, hand, hit);
+    }
+
+
+    // Block Entity BELOW
+//    @Override
+//    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+//        return null;
+//    }
+
+}
--- CS-Project/src/main/java/net/zihui/csprojmod/mobs/ModMobs.java
+++ CS-Project/src/main/java/net/zihui/csprojmod/mobs/ModMobs.java
@@ -6,12 +6,12 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.zihui.csprojmod.CSProjMain;
 
-public class ModMobs {
-    // Sets up deferred Register for entitytypes
-    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
-            DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, CSProjMain.MOD_ID);
-
-    // Sets up deferred register for boss entity
-    public static final DeferredRegister<Entity> TESTBOSS =
-            DeferredRegister.create(ForgeRegistries.
-}
+//public class ModMobs {
+//    // Sets up deferred Register for entitytypes
+//    public static final DeferredRegister<EntityType<?>> ENTITY_TYPES =
+//            DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, CSProjMain.MOD_ID);
+//
+//    // Sets up deferred register for boss entity
+//    public static final DeferredRegister<Entity> TESTBOSS =
+//            DeferredRegister.create(ForgeRegistries.
+//}
