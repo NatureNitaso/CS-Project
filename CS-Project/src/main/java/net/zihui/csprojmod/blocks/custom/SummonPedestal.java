b0334dc4fee9fae368e343e3edd356d43c1434a9
NatureNitaso <zihuilei001@gmail.com>
2024-12-10 17:48:15 -0800

    Implemented methods in SummonPedestal
    Methods n SummonPedestal include:
    use
    etc

--- CS-Project/src/main/java/net/zihui/csprojmod/blocks/custom/SummonPedestal.java
+++ CS-Project/src/main/java/net/zihui/csprojmod/blocks/custom/SummonPedestal.java
@@ -1,61 +1,65 @@
 package net.zihui.csprojmod.blocks.custom;
 
+import com.sun.net.httpserver.Authenticator;
+import net.minecraft.client.gui.components.ChatComponent;
 import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.commands.SummonCommand;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.client.event.CustomizeGuiOverlayEvent;
+import net.minecraftforge.event.level.NoteBlockEvent;
+import net.minecraftforge.eventbus.api.Event;
 import net.zihui.csprojmod.blocks.entity.SummonPedestalBlockEntity;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.w3c.dom.Text;
 
-public class SummonPedestal extends Block {
+public class SummonPedestal extends BaseEntityBlock {
     public Items summon;
-    public ItemStack storedItem = ItemStack.EMPTY;
 
     public SummonPedestal(Block.Properties properties) {
         super(properties);
     }
 
-    @Override
-    public boolean isRandomlyTicking(BlockState p_49921_) {
-        return super.isRandomlyTicking(p_49921_);
-    }
 
-    public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
-        return new SummonPedestalBlockEntity(pos, state);
-    }
-
-    // Overrides the interaction result method (what happens upon interacting)
     @Override
     public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-        // Makes a variable that identifies the held item of the player interacting with the block
-        ItemStack held = player.getItemInHand(hand);
-        // Grabs the # of items
-        int index = player.getItemInHand(hand).getCount();
+        ItemStack held = player.getItemInHand(hand); // grabs item that's in the players hand upon interaction
+        int index = held.getCount(); // Gets the # of items held in player's hand
 
-        BlockEntity be = level.getBlockEntity(pos);
-        // Checks to see if item matchs with it's requirements. 1. is running on server ride
-        // 2. held item is equal to summon item
-        // 3.
-        if (!level.isClientSide() && held.getItem() == Items.HEART_OF_THE_SEA) {
-            player.getItemInHand(hand).setCount(index - 1); //Takes away 1 item upon consumption
-            level.explode(player, pos.getX(), pos.getY(), pos.getZ(), 4.0F, true, Explosion.BlockInteraction.NONE);
-            return InteractionResult.CONSUME;
-        } else {
+        // Checks to see if this behavior is on the client or server side
+        if (!level.isClientSide()) {
+            if (held.getItem() == Items.HEART_OF_THE_SEA) {
+                index -= 1;
+                player.getItemInHand(hand).setCount(index);
+                return InteractionResult.SUCCESS;
+            } else {
+                index -= 1;
+                player.getItemInHand(hand).setCount(index);
+                return InteractionResult.SUCCESS;
 
-            player.getItemInHand(hand).setCount(index - 1);
-            storedItem = player.getItemInHand(hand);
-            be.saveToItem(storedItem);
-            return InteractionResult.CONSUME;
+            }
+            return InteractionResult.FAIL;
         }
+        return InteractionResult.FAIL;
+    }
 
-
+    @Override
+    public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+        return null;
     }
 }
 
--- CS-Project/src/main/java/net/zihui/csprojmod/blocks/entity/SummonPedestalBlockEntity.java
+++ CS-Project/src/main/java/net/zihui/csprojmod/blocks/entity/SummonPedestalBlockEntity.java
@@ -3,6 +3,7 @@ package net.zihui.csprojmod.blocks.entity;
 import ca.weblite.objc.Message;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -12,37 +13,83 @@ import net.minecraft.world.entity.ambient.Bat;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.fml.server.ServerMain;
 import net.minecraftforge.items.ItemStackHandler;
 import net.zihui.csprojmod.blocks.ModBlockEntity;
+import net.zihui.csprojmod.blocks.custom.SummonPedestal;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import javax.swing.plaf.basic.BasicComboBoxUI;
 import java.awt.*;
 import java.util.Objects;
+import java.util.Properties;
 
 public class SummonPedestalBlockEntity extends BlockEntity {
-    private ItemStack item = ItemStack.EMPTY; // This determines the item that is store by this block
 
-    public SummonPedestalBlockEntity(BlockPos pos, BlockState state) {
+    private ItemStack storedItem = ItemStack.EMPTY;
+    private Player p;
+
+    public SummonPedestalBlockEntity(BlockPos pos, BlockState state){
         super(ModBlockEntity.SUMMON_PEDESTAL_BLOCK_ENTITY.get(), pos, state);
+        System.out.println("Makes a Block Entity");
     }
 
-    // This sets the stored item within the block to whatever parameter is passed
+
+    public static void tick(Level world, BlockPos pos, BlockState state, SummonPedestalBlockEntity be) {
+        // This method runs every tick; you can use it to handle periodic logic
+    }
+
+    public InteractionResult using(BlockPos pos, Level level, BlockState state, Player player, InteractionHand hand) {
+        ItemStack held = player.getItemInHand(hand);
+        System.out.println("This method 'use' is being utilized");
+        int index;
+
+        if (held.getItem() == Items.HEART_OF_THE_SEA) {
+            sendMessage(player, held);
+        }
+        return InteractionResult.CONSUME;
+    }
+
+    public ItemStack getItem() {
+        return storedItem;
+    }
+
     public void setItem(ItemStack item) {
-        this.item = item;
-        this.setChanged();
+        this.storedItem = item;
+        setChanged(); // Updates block to sshow changes
     }
 
-    // Gets stored item
-    public ItemStack getStoredItem() {
-        return item;
+    public void sendMessage(Player player, ItemStack item) {
+        if (item.getItem() == Items.HEART_OF_THE_SEA) {
+            player.sendSystemMessage(net.minecraft.network.chat.Component.literal("Success "));
+        }
+        player.sendSystemMessage(Component.literal("Failed "));
     }
 
+
+
+
+
+
+//    // This sets the stored item within the block to whatever parameter is passed
+//    public void setItem(ItemStack item) {
+//        this.item = item;
+//        this.setChanged();
+//    }
+//
+//    // Gets stored item
+//    public ItemStack getStoredItem() {
+//        return item;
+//
 //    // Tells about player's interaction with block
 //    public InteractionResult use(Player player, InteractionHand hand) {
 //        // Checks to see if not player is running on Client side
