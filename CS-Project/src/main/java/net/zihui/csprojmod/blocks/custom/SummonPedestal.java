4223621f6a436759b3d899be865fef67f2afc518
NatureNitaso <zihuilei001@gmail.com>
2024-12-12 11:55:06 -0800

    Drops ItemEntity
    Drops ItemEntity if holding an item
    next: When pickingup item entity, it gives item too

--- CS-Project/src/main/java/net/zihui/csprojmod/blocks/custom/SummonPedestal.java
+++ CS-Project/src/main/java/net/zihui/csprojmod/blocks/custom/SummonPedestal.java
@@ -8,6 +8,7 @@ import net.minecraft.server.commands.SummonCommand;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -45,7 +46,7 @@ public class SummonPedestal extends BaseEntityBlock {
         int index = held.getCount(); // Gets the # of items held in player's hand
 
         // Checks to see if this behavior is on the client or server side
-        if (!level.isClientSide()) {
+        if (!level.isClientSide() && stored == ItemStack.EMPTY) {
             if (held.getItem() == Items.HEART_OF_THE_SEA) {
                 index -= 1;
                 player.getItemInHand(hand).setCount(index);
@@ -56,10 +57,11 @@ public class SummonPedestal extends BaseEntityBlock {
                 player.getItemInHand(hand).setCount(index);
                 setStoredItem(held, player, hand);
                 return InteractionResult.SUCCESS;
-
             }
+        } else {
+            returnStoredItem(level, pos, state);
+            return InteractionResult.SUCCESS;
         }
-        return InteractionResult.FAIL;
     }
 
     // Method to storeItem
@@ -69,28 +71,22 @@ public class SummonPedestal extends BaseEntityBlock {
         stored = item;
     }
 
-    // Method to return item to player
-    public void returnStoredItem(ItemStack item, Player player) {
-        player.getInventory().add(stored);
-        stored = ItemStack.EMPTY;
+    // Method that drops the item at the block pos in the world
+    public void returnStoredItem(Level level, BlockPos pos, BlockState state) {
+        if (!level.isClientSide) {
+            ItemEntity storedItemEntity = new ItemEntity(level, pos.getX(), pos.getY(),
+                    pos.getZ(), stored); // Makes new item entity based on what is stored within the obj
+            level.addFreshEntity(storedItemEntity); // Drops the entity of the item stored within the obj
+            stored = ItemStack.EMPTY; // Set stored obj variable back to empty
+        }
     }
 
     // Method for itemStored
 
-    public ItemStack manageStoredItem(BlockPos pos, Level level, BlockState state, Player player, InteractionHand hand) {
-        boolean isStoredEmpty = (stored == ItemStack.EMPTY); // Checks to see if stored == empty
-        boolean isHandEmpty = (player.getItemInHand(hand) != ItemStack.EMPTY); // Checks to see if hand is empty
-
-        if (isHandEmpty && isStoredEmpty){
-            return ItemStack.EMPTY;
-        }
-        else if (!isHandEmpty && isStoredEmpty) {
-            returnStoredItem(stored, player);
-        }
-
-
-        return stored;
-    }
+//    public ItemStack manageStoredItem(BlockPos pos, Level level, BlockState state, Player player, InteractionHand hand) {
+//        boolean isStoredEmpty = (stored == ItemStack.EMPTY); // Checks to see if stored == empty
+//        boolean isHandEmpty = (player.getItemInHand(hand) != ItemStack.EMPTY); // Checks to see if hand is empty
+//    }
 
 
 
