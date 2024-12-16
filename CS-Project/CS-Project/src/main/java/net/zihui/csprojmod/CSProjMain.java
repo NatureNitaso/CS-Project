9900c46acb2ab4f0df444a45ce86f91f769af522
NatureNitaso <zihuilei001@gmail.com>
2024-12-16 14:33:57 -0800

    1.19.4 Compatability solved

--- CS-Project/CS-Project/src/main/java/net/zihui/csprojmod/CSProjMain.java
+++ CS-Project/CS-Project/src/main/java/net/zihui/csprojmod/CSProjMain.java
@@ -2,9 +2,14 @@ package net.zihui.csprojmod;
 
 // imports up here //
 import com.mojang.logging.LogUtils;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.zihui.csprojmod.blocks.ModBlocks;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.zihui.csprojmod.init.ModCreativeTabs;
 import net.zihui.csprojmod.items.ModItems;
 import org.slf4j.Logger;
 import net.minecraftforge.fml.common.Mod;
@@ -14,27 +19,46 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(CSProjMain.MOD_ID)
 public class CSProjMain {
 
-    public static final Logger LOGGER = LogUtils.getLogger();
     public static final String MOD_ID = "csprojmod";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
     public CSProjMain() {
-        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         modEventBus.addListener(this::setup);
 
-
-
+        MinecraftForge.EVENT_BUS.register(this);
 
 
         // Calls to ModItems to register the items within there
         ModItems.ITEMS.register(modEventBus);
         // Calls to ModBlocks to register the blocks made there
-        ModBlocks.BLOCKS.register(modEventBus);
+//        ModBlocks.BLOCKS.register(modEventBus);
 
-        MinecraftForge.EVENT_BUS.register(this);
+
     }
 
     private void setup(final FMLCommonSetupEvent event) {
 
+    }
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+
+
+        }
+    }
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {
+        if (event.getTab() == ModCreativeTabs.CSPROJ_MOD) {
+            event.accept(ModItems.CRACK);
+            event.accept(ModItems.TESTOBJ);
+
+
+
+        }
     }
 }
--- CS-Project/CS-Project/src/main/java/net/zihui/csprojmod/items/ModItems.java
+++ CS-Project/CS-Project/src/main/java/net/zihui/csprojmod/items/ModItems.java
@@ -12,7 +12,7 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.zihui.csprojmod.CSProjMain;
-import net.zihui.csprojmod.init.ModCreativeTab;
+import net.zihui.csprojmod.init.ModCreativeTabs;
 
 public class ModItems {
     // Sets up register for deferred register method
@@ -22,20 +22,17 @@ public class ModItems {
 
     // Register first item
     public static final RegistryObject<Item> TESTOBJ = ITEMS.register("testobj",
-            () -> new Item(new Item.Properties().tab(ModCreativeTab.TAB_MODDED)));
+            () -> new Item(new Item.Properties()));
 
     // Registers food, then adds food values onto the item
     public static final RegistryObject<Item> CRACK = ITEMS.register("crack",
-            () -> new Item(new Item.Properties().tab(ModCreativeTab.TAB_MODDED)
+            () -> new Item(new Item.Properties()
                     .food(new FoodProperties.Builder().nutrition(10)
                             .saturationMod(1).effect
                                     (() -> new MobEffectInstance
                                             (MobEffects.DAMAGE_BOOST, 1200, 3), 1F).
                             effect(() -> new MobEffectInstance(MobEffects.BLINDNESS, 1500, 2),1F).
                             effect(() -> new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 1500, 3),1F).build()))); // Initiates the process of making this item
-
-    public static final RegistryObject<Item> kNIGHTSWORD = ITEMS.register("knightsword",
-            () -> new KnightSword(new Item.Properties().durability(119), ModCreativeTab.TAB_MODDED));
 
 
 
